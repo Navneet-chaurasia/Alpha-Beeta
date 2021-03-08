8e5718ba57e073479263ed75c2d14723dd42e449
navneetkumar <navneetc486@gmail.com>
2021-03-08 11:01:23 +0530

    node.js support added

--- public/global_components/navbar.js
+++ public/global_components/navbar.js
@@ -4,18 +4,22 @@ This is a prefered method of creating components
 2.class must declare all method static
 3. class must have a method called "render()"
 4. all the html code should be placed inside of this render method, or whatever way you prefer, 
-   but this method must return html code. => Navneet chaurasia
+   but this method must return html code.
+   
+5. write style sheet inside "styleSheet function which is static" 
+   Navneet chaurasia
 */
 
 
 class NavBar {
 
+
+    
+
+
     static render(){
 
-        return (`<>
-        
-        <h1>hello component tetsing</h1>
-        </>`);
+        return  `<div id="navbar" style="height:50px; background-color:grey;position :static"></div>`;
     }
 
 }
