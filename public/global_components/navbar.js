70ba717ac84b42c2965809f9a4b35ebb16c588d2
navneetkumar <navneetc486@gmail.com>
2021-03-08 23:08:14 +0530

    navbar updated

--- public/global_components/navbar.js
+++ public/global_components/navbar.js
@@ -21,12 +21,39 @@ class NavBar {
 
         return  `
         <style>
-        #navbar{
-            height:50px;
-            background-color:grey;
-        }
+        .topnav {
+            background-color: blue;
+            overflow: hidden;
+          }
+          
+          /* Style the links inside the navigation bar */
+          .topnav a {
+            float: left;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            font-size: 17px;
+          }
+          
+          /* Change the color of links on hover */
+          .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+          }
+          
+          /* Add a color to the active/current link */
+          .topnav a.active {
+            background-color:green;
+            color: white;
+          }
         </style>
-        <div id="navbar" ></div>`;
+        <div class="topnav">
+  <a class="active" href="#home">Wonder-Games</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>`;
     }
 
 }
