4143c0c3832159783467022f0533aeb9b732f158
navneetkumar <navneetc486@gmail.com>
2021-03-15 18:02:22 +0530

    some documentation change

--- public/global_components/NavBar/navbar.js
+++ public/global_components/NavBar/navbar.js
@@ -1,13 +1,3 @@
-/*
-This is a prefered method of creating components
-1. make a class (Name of component)
-2.class must declare all method static
-3. class must have a method called "render()"
-4. import "loadFile" function
-5. provide correct path of component.html file (path should be correct)
-6. write style sheet inside <style></style> tags in the same html file
-
-*/
 
 
 var shadow;
